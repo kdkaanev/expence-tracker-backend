3c3b8eedbad3fecb05f2b7b512ad4553b6753245
kdkaanev <kdkaane@github.io>
2025-09-29 21:17:15 +0300

    debug true

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "Тrue")== "True"
+#DEBUG = os.getenv("DJANGO_DEBUG", "Тrue")== "True"
+DEBUG = True
 
 
 ALLOWED_HOSTS = ["expence.azurewebsites.net", "localhost", "127.0.0.1"]
