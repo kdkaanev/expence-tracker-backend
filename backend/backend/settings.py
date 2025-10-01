368ebac89e3c35951a385cd16972d9a3ccfebbab
Kancho <103933652+kdkaanev@users.noreply.github.com>
2025-10-01 08:39:10 +0300

    Fix typo in DEBUG variable assignment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-EBUG = os.getenv("DJANGO_DEBUG", "Тrue")== "True"
+DEBUG = os.getenv("DJANGO_DEBUG", "Тrue")== "True"
 #DEBUG = True
 
 
