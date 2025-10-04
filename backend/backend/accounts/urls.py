ed2c27ab075963894a3e788e1c004b5a9724f68d
kdkaanev <kdkaane@github.io>
2025-10-04 19:13:39 +0300

    fix account.urls

--- backend/backend/accounts/urls.py
+++ backend/backend/accounts/urls.py
@@ -1,6 +1,8 @@
-from .views import me
+from .views import me, me_profile
 from django.urls import path
 
 urlpatterns = [
     path('', me, name='me'),
+    path('profile/', me_profile, name='me_profile'),
+
 ]
