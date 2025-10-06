817ec450ae64f2bafef6558a126b4a179d98d1c1
kdkaanev <kdkaane@github.io>
2025-10-06 18:14:01 +0300

    fix category type

--- backend/backend/api/models.py
+++ backend/backend/api/models.py
@@ -1,19 +1,23 @@
-
 from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 CATEGORY_CHOICES = [
-    ('expense', 'Expense'),
-    ('income', 'Income'),
+    ('Expense', 'expense'),
+    ('Income', 'income'),
 ]
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    type = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="categories")
+    type = models.CharField(
+        max_length=10,
+        choices=CATEGORY_CHOICES, default= False
+    )
 
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="categories")
 
     def __str__(self):
         return self.name
