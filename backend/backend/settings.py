021c408059a76299a179f8f04c277ab916004426
kdkaanev <kdkaane@github.io>
2025-11-12 19:36:03 +0200

    up

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ["expencetracker-aqhzd2fvfnhaaydd.northeurope-01.azurewebsites.net", "localhost", "127.0.0.1", "purple-bush-04800311e.3.azurestaticapps.net"]
+ALLOWED_HOSTS = ["expence-epcwcbdwemaed5fj.canadacentral-01.azurewebsites.net", "localhost", "127.0.0.1", "purple-bush-04800311e.3.azurestaticapps.net"]
 
 # Application definition
 
@@ -69,7 +69,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'https://expencetracker-aqhzd2fvfnhaaydd.northeurope-01.azurewebsites.net',
+    'https://expence-epcwcbdwemaed5fj.canadacentral-01.azurewebsites.net',
     'https://purple-bush-04800311e.3.azurestaticapps.net'
 ]
 
